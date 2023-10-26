457319ffc9504d210f42c367f23cc5498e345d79
Santos <IcTxDiogo@gmail.com>
2023-10-26 17:59:53 -0400

    feat: add shadcn

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
     return (
         <>
             <div>{"home"}</div>
-            <Link href={"/example-page"}>Example Page</Link>
+            <Link href={"/example-page"}>
+                <Button variant={"ghost"}>{"Go to example page"}</Button>
+            </Link>
         </>
     );
 }
--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,11 +1,12 @@
-import "@/styles/globals.css";
-
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import { headers } from "next/headers";
+import { type ReactNode } from "react";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
 
-const inter = Inter({
+const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
 });
@@ -16,10 +17,19 @@ export const metadata = {
     icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+type RootLayoutProps = {
+    children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
-            <body className={`font-sans ${inter.variable}`}>
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased",
+                    fontSans.variable,
+                )}
+            >
                 <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
             </body>
         </html>
