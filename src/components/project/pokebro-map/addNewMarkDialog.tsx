e346de1719a35f7208d06d9227c35de64b23ac67
Diogo frança dos santos <ictxdiogo@gmail.com>
2023-11-14 12:57:32 -0400

    feat: add formItemRender and finish up the form with the log

--- src/components/project/pokebro-map/addNewMarkDialog.tsx
+++ src/components/project/pokebro-map/addNewMarkDialog.tsx
@@ -1,47 +1,39 @@
+import { useForm } from "react-hook-form";
 import { Plus } from "lucide-react";
+import { z } from "zod";
+
+import FormItemRender from "@/components/form/formItemRender";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import {
     Dialog,
     DialogContent,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form";
 
 const formSchema = z.object({
-    Name: z.string().min(3),
-    PositionX: z.number(),
-    PositionY: z.number(),
-    Floor: z.number(),
-    Information: z.string(),
-    Type: z.string(),
+    name: z.string().min(3),
+    posX: z.coerce.number(),
+    posY: z.coerce.number(),
+    floor: z.coerce.number(),
+    information: z.string(),
+    type: z.string(),
 });
 
 export default function AddNewMarkDialog() {
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            Name: "",
-            PositionX: 0,
-            PositionY: 0,
-            Floor: 0,
-            Information: "",
-            Type: "",
+            name: "",
+            posX: 0,
+            posY: 0,
+            floor: 0,
+            information: "",
+            type: "",
         },
     });
 
@@ -64,61 +56,49 @@ export default function AddNewMarkDialog() {
                     <DialogHeader>
                         <DialogTitle>New marker</DialogTitle>
                     </DialogHeader>
-                    <div className="grid gap-4 py-4">
-                        <Form {...form}>
-                            <form onSubmit={form.handleSubmit(onSubmit)} className={"space-y-8"}>
-                                <FormField
-                                    control={form.control}
-                                    name={"Name"}
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Name</FormLabel>
-                                            <FormControl>
-                                                <Input {...field} />
-                                            </FormControl>
-                                            <FormDescription>
-                                                The name of the marker
-                                            </FormDescription>
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </form>
-                        </Form>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="posX" className="text-right">
-                                Position X
-                            </Label>
-                            <Input id="posX" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="posY" className="text-right">
-                                Position Y
-                            </Label>
-                            <Input id="posY" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="floor" className="text-right">
-                                Floor
-                            </Label>
-                            <Input id="floor" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="information" className="text-right">
-                                Information
-                            </Label>
-                            <Input id="information" className="col-span-3" />
-                        </div>
-                        <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="type" className="text-right">
-                                Type
-                            </Label>
-                            <Input id="type" className="col-span-3" />
-                        </div>
-                    </div>
-                    <DialogFooter>
-                        <Button type="submit">Save changes</Button>
-                    </DialogFooter>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormItemRender
+                                form={form}
+                                name={"name"}
+                                label={"Name"}
+                                renderItem={(field) => <Input {...field} />}
+                            />
+                            <FormItemRender
+                                form={form}
+                                name={"posX"}
+                                label={"Position X"}
+                                renderItem={(field) => <Input {...field} type={"number"} />}
+                            />
+                            <FormItemRender
+                                form={form}
+                                name={"posY"}
+                                label={"Position Y"}
+                                renderItem={(field) => <Input {...field} type={"number"} />}
+                            />
+                            <FormItemRender
+                                form={form}
+                                name={"floor"}
+                                label={"Floor"}
+                                renderItem={(field) => <Input {...field} type={"number"} />}
+                            />
+                            <FormItemRender
+                                form={form}
+                                name={"information"}
+                                label={"Information"}
+                                renderItem={(field) => <Input {...field} />}
+                            />
+                            <FormItemRender
+                                form={form}
+                                name={"type"}
+                                label={"Type"}
+                                renderItem={(field) => <Input {...field} />}
+                            />
+                            <div className={"flex justify-end pt-4"}>
+                                <Button type="submit">Save changes</Button>
+                            </div>
+                        </form>
+                    </Form>
                 </DialogContent>
             </Dialog>
         </>
