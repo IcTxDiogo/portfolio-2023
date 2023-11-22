c2cde16ad0bedb79a6b1f069d8018ea3ce1622aa
Santos <ictxdiogo@gmail.com>
2023-11-21 22:28:41 -0400

    feat: add find markers but need some polish

--- src/components/project/pokebro-map/showFindDialog.tsx
+++ src/components/project/pokebro-map/showFindDialog.tsx
@@ -9,7 +9,8 @@ import {
     CommandSeparator,
 } from "@/components/ui/command";
 import { type MapMarkers } from "@/app/(pages)/project/pokebro-map/page";
-import { Building } from "lucide-react";
+import { Building, Coins } from "lucide-react";
+import { api } from "@/trpc/react";
 
 type ShowFindDialogProps = {
     cityMarks: MapMarkers;
@@ -19,6 +20,24 @@ type ShowFindDialogProps = {
 export default function ShowFindDialog({ cityMarks, handleSelectMarker }: ShowFindDialogProps) {
     const [findDialog, setFindDialog] = useState(false);
     const [dialogSearchValue, setDialogSearchValue] = useState("");
+    const [debouncedSearchValue, setDebouncedSearchValue] = useState(dialogSearchValue);
+
+    const searchResult = api.pokebroMap.markSearch.useQuery({
+        query: debouncedSearchValue,
+        type: ["trials"],
+    });
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedSearchValue(dialogSearchValue);
+        }, 500); // 500ms delay
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [dialogSearchValue]);
+
+    const result = searchResult.data;
 
     useEffect(() => {
         function down(e: KeyboardEvent) {
@@ -34,28 +53,51 @@ export default function ShowFindDialog({ cityMarks, handleSelectMarker }: ShowFi
 
     return (
         <div onWheel={(e) => e.stopPropagation()} onMouseDown={(e) => e.stopPropagation()}>
-            <CommandDialog open={findDialog} onOpenChange={setFindDialog}>
+            <CommandDialog
+                open={findDialog}
+                onOpenChange={setFindDialog}
+                shouldFilter={!(result !== undefined && result.length > 0)}
+            >
                 <CommandInput
                     placeholder="Type a command or search..."
                     value={dialogSearchValue}
                     onValueChange={setDialogSearchValue}
                 />
                 <CommandList>
-                    <CommandEmpty>No results found.</CommandEmpty>
-                    <CommandGroup heading="Cities">
-                        {cityMarks.map((item, index) => (
+                    <CommandEmpty>
+                        {dialogSearchValue.length < 3
+                            ? "type a less 3 character"
+                            : "No results found"}
+                    </CommandEmpty>
+                    {result !== undefined && result.length > 0 ? (
+                        result.map((mark) => (
                             <CommandItem
-                                key={index}
+                                key={mark.id}
                                 onSelect={() => {
-                                    handleSelectMarker(item.posX, item.posY, item.floor);
+                                    handleSelectMarker(mark.posX, mark.posY, mark.floor);
                                     setFindDialog(false);
                                 }}
                             >
-                                <Building />
-                                <span>{item.name}</span>
+                                <Coins />
+                                <span>{mark.name}</span>
                             </CommandItem>
-                        ))}
-                    </CommandGroup>
+                        ))
+                    ) : (
+                        <CommandGroup heading="Cities">
+                            {cityMarks.map((item, index) => (
+                                <CommandItem
+                                    key={index}
+                                    onSelect={() => {
+                                        handleSelectMarker(item.posX, item.posY, item.floor);
+                                        setFindDialog(false);
+                                    }}
+                                >
+                                    <Building />
+                                    <span>{item.name}</span>
+                                </CommandItem>
+                            ))}
+                        </CommandGroup>
+                    )}
                     <CommandSeparator />
                 </CommandList>
             </CommandDialog>
